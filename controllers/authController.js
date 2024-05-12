2a5a276272a56d99165fb1d8c14ea643dbeded3a
unorji kingsley <unorjikingsley@gmail.com>
2024-05-12 16:21:16 +0100

    added login controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,9 @@ import { StatusCodes } from "http-status-codes";
 import prisma from "../DB/db.config.js";
 import cloudinary from 'cloudinary';
 import { formatImage } from "../middleware/multerMiddleware.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtil.js";
 
 export const registerUser = async (req, res) => {
   try {
@@ -52,11 +55,15 @@ export const registerUser = async (req, res) => {
       }
     }
 
+    const hashedPassword = await hashPassword(req.body.password)
+    req.body.password = hashedPassword
+
     const newUser = await prisma.user.create({
       data: {
         ...user,
         role: requestedRole,
         image: user.image,
+        password: hashedPassword
       },
     })
 
@@ -69,3 +76,31 @@ export const registerUser = async (req, res) => {
   }
 }
 
+export const loginUser = async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: req.body.email}
+    })
+
+    const isValidUser = user && (await comparePassword(req.body.password, user.password))
+
+    if(!isValidUser) throw new UnauthenticatedError('Invalid credentials')
+
+    const token = createJWT({ userId: user.id, role: user.role })
+
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + oneDay),
+      secure: process.env.NODE_ENV === 'production',
+    })
+
+    res.status(StatusCodes.OK).json({ user })
+  } catch (error) {
+    console.log('Error:', error)
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: 'Internal Server Error' })
+  }
+} 
