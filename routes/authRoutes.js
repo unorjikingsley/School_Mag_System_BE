ba682ab583e4a3dcf7c81a6e352d1da3a86e7d67
unorji kingsley <unorjikingsley@gmail.com>
2024-05-12 16:21:56 +0100

    routes for login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, loginUser } from "../controllers/authController.js";
 
 import upload from "../middleware/multerMiddleware.js";
 
 const router = Router();
 
+// import rateLimiter from 'express-rate-limit'
+
+// const apiLimiter = rateLimiter({
+//   windowMs: 15 * 60 * 1000,
+//   max: 5,
+//   message: { msg: 'IP rate limit exceeded, retry in 15 minutes' },
+// })
+
 router.post('/register', upload.single('image'), registerUser);
+router.post('/login', loginUser);
 
 export default router;
